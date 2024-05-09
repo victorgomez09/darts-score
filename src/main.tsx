aa917b2d9b5cfccd51c4f5d32af3be2d086bd414
Víctor Gómez <victorgomez1209@outlook.es>
2024-05-09 20:13:48 +0200

    move app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import './main.css';
+import "./main.css";
 import App from "./pages/App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainMenu from "./menus/Menu.tsx";
 import Multiplayer from "./modes/Multiplayer.tsx";
 import Singleplayer from "./modes/Singleplayer.tsx";
@@ -8,20 +8,25 @@ import Settings, { DART_THEME } from "./Settings.tsx";
 
 function App() {
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", localStorage.getItem(DART_THEME) || "default");
-  }, [])
+    document.documentElement.setAttribute(
+      "data-theme",
+      localStorage.getItem(DART_THEME) || "default"
+    );
+  }, []);
 
   return (
     <div className="flex flex-1 bg-base-300 w-full h-full">
-      <Routes>
-        <Route path="/" element={<MainMenu />} />
-        <Route path="/singleplayer" element={<Singleplayer />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainMenu />} />
+          <Route path="/singleplayer" element={<Singleplayer />} />
 
-        <Route path="/multiplayer" element={<Multiplayer />} />
-        <Route path="/tournament" element={<Tournament />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes >
-    </div >
+          <Route path="/multiplayer" element={<Multiplayer />} />
+          <Route path="/tournament" element={<Tournament />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
